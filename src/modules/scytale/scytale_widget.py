f849bddf04391d435c3bd390abe64b7c9a936d02
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-03-11 18:54:03 +0700

    Added Scytal cipher

--- src/modules/scytale/scytale_widget.py
+++ src/modules/scytale/scytale_widget.py
@@ -1,11 +1,10 @@
-import sys
-
 from PyQt6.QtWidgets import (
-    QApplication,
-    QWidget
+    QWidget,
+    QMessageBox
 )
 
-from .scytale_ui import Ui_scytale
+from src.modules.scytale.scytale_ui import Ui_scytale
+from src.crypto.symmetric import scytale
 
 
 class ScytaleWidget(QWidget):
@@ -15,12 +14,32 @@ class ScytaleWidget(QWidget):
         self.ui.setupUi(self)
 
         self.title = "Scytale"
-        self.group = "Symmetric encryption"
 
+        self.ui.check_box_columns.stateChanged.connect(self.check_box_check)
+        self.ui.button_make.clicked.connect(self.button_make_clicked)
 
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    window = ScytaleWidget()
-    window.show()
+    def check_box_check(self) -> None:
+        """Scytale | (Slot) Method for activating/deactivating a checkbox."""
+        if self.ui.check_box_columns.isChecked():
+            self.ui.spin_box_columns.setDisabled(False)
+            self.ui.check_box_columns.setStyleSheet("color: white")
+        else:
+            self.ui.spin_box_columns.setDisabled(True)
+            self.ui.check_box_columns.setStyleSheet("color: grey")
 
-    sys.exit(app.exec())
+    def button_make_clicked(self) -> None:
+        """Scytale | (Slot) Method for handling button click. (Encryption/decryption)"""
+        try:
+            processed_text = scytale.make(
+                text=self.ui.text_edit_input.toPlainText(),
+                n=self.ui.spin_box_rows.value(),
+                m=self.ui.spin_box_columns.value(),
+                auto_m=not self.ui.check_box_columns.isChecked(),
+                mode=self.ui.combo_box_mode.currentText().lower()
+            )
+
+        except scytale.ScytaleError as e:
+            QMessageBox.warning(self, "Warning!", e.args[0])
+            return
+
+        self.ui.text_edit_output.setText(processed_text)

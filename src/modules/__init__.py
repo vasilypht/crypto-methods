05566cbc0f2bf91a3762787128d28dfa4ff9b54c
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-03-11 19:29:44 +0700

    Fixed module name

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -3,7 +3,7 @@ from typing import Final
 from .atbash.atbash_widget import AtbashWidget
 from .scytale.scytale_widget import ScytaleWidget
 from .polybius_square.polybius_square_widget import PolybiusSquareWidget
-from .caesar.caesar import CaesarWidget
+from .caesar.caesar_widget import CaesarWidget
 
 
 WIDGETS_CIPHERS: Final = (

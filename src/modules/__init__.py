57f9e284558721478becce2aac5094bc1350fce0
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-03-11 19:16:56 +0700

    Added Polybius square cipher

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -2,8 +2,10 @@ from typing import Final
 
 from .atbash.atbash_widget import AtbashWidget
 from .scytale.scytale_widget import ScytaleWidget
+from .polybius_square.polybius_square_widget import PolybiusSquareWidget
 
 WIDGETS_CIPHERS: Final = (
     AtbashWidget,
-    ScytaleWidget
+    ScytaleWidget,
+    PolybiusSquareWidget
 )

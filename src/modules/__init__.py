08edfe2e5a69980843a5e3704f0defcbb2027d13
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-03-11 20:00:45 +0700

    Added cipher Richelieu

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -5,6 +5,7 @@ from .scytale.scytale_widget import ScytaleWidget
 from .polybius_square.polybius_square_widget import PolybiusSquareWidget
 from .caesar.caesar_widget import CaesarWidget
 from .cardan_grille.cardan_grille_widget import CardanGrilleWidget
+from .richelieu.richelieu_widget import RichelieuWidget
 
 
 WIDGETS_CIPHERS: Final = (
@@ -12,5 +13,6 @@ WIDGETS_CIPHERS: Final = (
     ScytaleWidget,
     PolybiusSquareWidget,
     CaesarWidget,
-    CardanGrilleWidget
+    CardanGrilleWidget,
+    RichelieuWidget
 )

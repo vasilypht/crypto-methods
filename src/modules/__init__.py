8955b9ca6801986d6c6713ed3d0f929b6cea0fb9
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-03-11 23:44:39 +0700

    Added cipher Gronsfeld

--- src/modules/__init__.py
+++ src/modules/__init__.py
@@ -7,6 +7,7 @@ from .caesar.caesar_widget import CaesarWidget
 from .cardan_grille.cardan_grille_widget import CardanGrilleWidget
 from .richelieu.richelieu_widget import RichelieuWidget
 from .alberti_disc.alberti_disc_widget import AlbertiDiscWidget
+from .gronsfeld.gronsfeld_widget import GronsfeldWidget
 
 
 WIDGETS_CIPHERS: Final = (
@@ -16,5 +17,6 @@ WIDGETS_CIPHERS: Final = (
     CaesarWidget,
     CardanGrilleWidget,
     RichelieuWidget,
-    AlbertiDiscWidget
+    AlbertiDiscWidget,
+    GronsfeldWidget
 )

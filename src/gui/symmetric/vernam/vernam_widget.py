817066fd973b59b795dc338cf412ba8f6369367e
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-03-15 21:37:02 +0700

    Added the ability to save, load and generate a key

--- src/gui/symmetric/vernam/vernam_widget.py
+++ src/gui/symmetric/vernam/vernam_widget.py
@@ -1,10 +1,21 @@
 from PyQt6.QtWidgets import (
     QWidget,
-    QMessageBox
+    QMessageBox,
+    QMenu,
+    QFileDialog
 )
+from PyQt6.QtGui import (
+    QIcon,
+)
+from PyQt6.QtCore import (
+    QSize
+)
+import numpy as np
 
 from .vernam_ui import Ui_vernam
 from src.crypto.symmetric import vernam
+
+MAX_BYTES_READ = 1024
 
 
 class VernamWidget(QWidget):
@@ -12,10 +23,68 @@ class VernamWidget(QWidget):
         super(VernamWidget, self).__init__()
         self.ui = Ui_vernam()
         self.ui.setupUi(self)
-
         self.title = "Vernam"
+        self.__init_style()
 
         self.ui.button_make.clicked.connect(self.button_make_clicked)
+
+        self.ui.button_settings.setIconSize(QSize(24, 24))
+        self.ui.button_settings.setIcon(QIcon("icons:dots.png"))
+
+        # Context menu
+        menu = QMenu()
+        menu.addAction("Generate key", self.action_clicked_gen_key)
+        menu.addSeparator()
+        menu.addAction("Save key", self.action_clicked_save_key)
+        menu.addAction("Load key", self.action_clicked_load_key)
+        self.ui.button_settings.setMenu(menu)
+
+    def action_clicked_gen_key(self):
+        if not self.ui.text_edit_input.toPlainText():
+            QMessageBox.warning(self, "Warning!", "Input text field is empty!")
+            return
+
+        sample_bytes = np.random.randint(0, 256, len(self.ui.text_edit_input.toPlainText()))
+        key_bytes = bytes(list(sample_bytes))
+        self.ui.line_edit_key.setText(key_bytes.decode(encoding="KOI8-r"))
+
+    def action_clicked_save_key(self):
+        if not self.ui.line_edit_key.text():
+            QMessageBox.warning(self, "Warning!", "The field with the key is empty!")
+            return
+
+        filename, _ = QFileDialog.getSaveFileName(
+            parent=self,
+            caption="Save a key file",
+            directory="",
+            filter="Text File (*.txt)",
+            initialFilter="Text File (*.txt)"
+        )
+
+        try:
+            with open(filename, "w") as f_out:
+                f_out.write(self.ui.line_edit_key.text())
+
+        except OSError as e:
+            QMessageBox.warning(self, "Warning!", e.args[0])
+            return
+
+    def action_clicked_load_key(self):
+        filename, _ = QFileDialog.getOpenFileName(
+            parent=self,
+            caption="Open a key file",
+            directory="",
+            filter="Text File (*.txt)",
+            initialFilter="Text File (*.txt)"
+        )
+
+        try:
+            with open(filename, "r") as f_out:
+                self.ui.line_edit_key.setText(f_out.read(MAX_BYTES_READ))
+
+        except OSError as e:
+            QMessageBox.warning(self, "Warning!", e.args[0])
+            return
 
     def button_make_clicked(self) -> None:
         """Vernam | (Slot) Method for handling button click. (Encryption/decryption)"""
@@ -30,3 +99,13 @@ class VernamWidget(QWidget):
             return
 
         self.ui.text_edit_output.setText(processed_text)
+
+    def __init_style(self):
+        self.setStyleSheet("""
+            QPushButton#button_settings {
+                background: transparent;
+            }
+            QPushButton::menu-indicator {
+                image: none;
+            }
+        """)

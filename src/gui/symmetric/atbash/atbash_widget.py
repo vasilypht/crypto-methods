5a3db8782c4214fe301dc320948a96479934eb8d
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-03-27 17:25:21 +0700

    Redesigned ui for Atbash widget

--- src/gui/symmetric/atbash/atbash_widget.py
+++ src/gui/symmetric/atbash/atbash_widget.py
@@ -19,13 +19,18 @@ class AtbashWidget(QWidget):
 
     def button_make_clicked(self):
         """Atbash | (Slot) Method for handling button click. (Encryption/decryption)"""
-        try:
-            processed_text = atbash.make(
-                text=self.ui.text_edit_input.toPlainText()
-            )
+        match self.ui.tab_widget.currentWidget():
+            case self.ui.tab_text:
+                try:
+                    processed_text = atbash.make(
+                        text=self.ui.text_edit_input.toPlainText()
+                    )
 
-        except atbash.AtbashError as e:
-            QMessageBox.warning(self, "Warning!", e.args[0])
-            return
+                except atbash.AtbashError as e:
+                    QMessageBox.warning(self, "Warning!", e.args[0])
+                    return
 
-        self.ui.text_edit_output.setText(processed_text)
+                self.ui.text_edit_output.setText(processed_text)
+
+            case _:
+                return

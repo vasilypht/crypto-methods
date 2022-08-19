b9d717c45eab789d0048ae4b3bbdccb8aef3333c
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-08-19 23:50:35 +0700

    Added documentation to the alberti_disc.py module, as well as to the corresponding widget.

--- app/crypto/symmetric/alberti_disc.py
+++ app/crypto/symmetric/alberti_disc.py
@@ -1,3 +1,4 @@
+# This module contains the implementation of the cipher "Alberti's Disk"
 import re
 
 from ..utils import get_alphabet_by_letter
@@ -6,11 +7,26 @@ from ..common import EncProc
 
 
 class AlbertiError(Exception):
+    """The exception that is thrown when an error occurs in the Alberti class"""
     pass
 
 
 class Alberti:
-    def __init__(self, key: str, step: int = 0, shift: int = 0):
+    def __init__(self, key: str, step: int = 0, shift: int = 0) -> None:
+        """
+        Alberti class constructor.
+
+        Args:
+            key: a string containing only characters of the English alphabet, or only the Russian alphabet.
+                It is allowed to use key characters in different case. If the key contains numbers, special
+                characters, or characters from other alphabets, then the AlbertiError exception will be raised.
+
+            step: encryption step. The value by which the disk will be shifted after each iteration.
+                This value must be positive. If this value is negative, an AlbertiError exception will be thrown.
+
+            shift: initial shift of the internal alphabet. This value must be positive. If this value
+                is negative, an AlbertiError exception will be thrown.
+        """
         if not key:
             raise AlbertiError("The key is missing!")
 
@@ -30,25 +46,43 @@ class Alberti:
         self.shift = shift
 
     def _transform(self, text: str, enc_proc: EncProc) -> str:
+        """
+        Data encryption/decryption method.
+
+        Args:
+            text: the string to be encrypted or decrypted.
+
+            enc_proc: parameter responsible for the process of data encryption (encryption and decryption).
+
+        Returns:
+            Encrypted or decrypted string.
+        """
         if not text:
             raise AlbertiError("Input text is empty!")
 
+        # Since the alphabet consists of letters of the same alphabet,
+        # we get the alphabet by the first letter of the key.
         _, external_alphabet = get_alphabet_by_letter(self.key[0], ALPHABET_TABLE)
 
+        # We form the internal alphabet - the key + the remaining letters of the alphabet.
         internal_alphabet = ""
         for letter in self.key.lower() + external_alphabet:
+            # We add letters from the key, excluding repetitions, and then
+            # supplement with letters from the alphabet.
             if letter not in internal_alphabet:
                 internal_alphabet += letter
 
-        # Making a shift in the internal alphabet
+        # Shifting the internal alphabet to the right.
         internal_alphabet = internal_alphabet[self.shift::] + internal_alphabet[:self.shift:]
 
         match enc_proc:
             case EncProc.ENCRYPT:
+                # The encryption process will be carried out by shifting the internal alphabet to the right.
                 key_sign = 1
 
             case EncProc.DECRYPT:
-                # swap alphabets
+                # The decryption process will take place by shifting the internal alphabet to the left,
+                # that is, the reverse process. To do this, swap the internal and external disks.
                 internal_alphabet, external_alphabet = external_alphabet, internal_alphabet
                 key_sign = -1
 
@@ -61,12 +95,16 @@ class Alberti:
         for i in range(len(text)):
             letter = text_list[i]
 
+            # We are looking for letters of the internal alphabet on an external disk.
+            # If the position is -1, then the character does not belong to the alphabet - we skip the iteration.
             if (letter_pos := external_alphabet.find(letter.lower())) == -1:
                 continue
 
+            # We consider a new position taking into account the shift of the alphabet.
             new_letter_pos = (letter_pos + internal_shift) % len(internal_alphabet)
             new_letter = internal_alphabet[new_letter_pos]
 
+            # Update the alphabet shift value.
             internal_shift = (internal_shift + self.step * key_sign) % len(internal_alphabet)
 
             if letter.isupper():
@@ -77,12 +115,42 @@ class Alberti:
         return "".join(text_list)
 
     def encrypt(self, text: str) -> str:
+        """
+        Method - interface for encrypting input data.
+
+        Args:
+            text: the string to be encrypted.
+
+        Returns:
+            Encrypted string.
+        """
         return self._transform(text, EncProc.ENCRYPT)
 
     def decrypt(self, text: str) -> str:
+        """
+        Method - interface for decrypting input data.
+
+        Args:
+            text: the string to be decrypted.
+
+        Returns:
+            Decrypted string.
+        """
         return self._transform(text, EncProc.DECRYPT)
 
     def make(self, text: str, enc_proc: EncProc = EncProc.ENCRYPT) -> str:
+        """
+        Method - interface for encrypting/decrypting input data.
+
+        Args:
+            text: the string to be encrypted or decrypted.
+
+            enc_proc: parameter responsible for the process of data encryption (encryption and decryption).
+                If the data object is of a different type, then an exception will be raised AlbertiError.
+
+        Returns:
+
+        """
         match enc_proc:
             case EncProc.ENCRYPT:
                 return self.encrypt(text)
--- app/gui/symmetric/alberti_disc/alberti_disc_widget.py
+++ app/gui/symmetric/alberti_disc/alberti_disc_widget.py
@@ -1,3 +1,5 @@
+# This module contains the implementation of the widget for working
+# with the encryption algorithm "Alberti's Disk".
 from PyQt6.QtCore import QRegularExpression as QRegExp
 from PyQt6.QtWidgets import QMessageBox
 from PyQt6.QtGui import QRegularExpressionValidator as QRegExpVal
@@ -12,12 +14,17 @@ from app.gui.widgets import BaseQWidget
 
 
 class AlbertiDiscWidget(BaseQWidget):
-    def __init__(self) -> None:
-        super(AlbertiDiscWidget, self).__init__()
+    def __init__(self, *args, **kwargs) -> None:
+        """AlbertiDiscWidget class constructor"""
+        super(AlbertiDiscWidget, self).__init__(*args, **kwargs)
         self.ui = Ui_AlbertiDisk()
         self.ui.setupUi(self)
 
+        # Define the name of the widget that will be displayed in the list of widgets.
         self.title = "Alberti disc"
+
+        # Initialization of possible encryption processes.
+        self.ui.combo_box_enc_proc.addItems((item.name.capitalize() for item in EncProc))
 
         # We set validation so that only Russian characters can be entered, or only English characters.
         self.ui.line_edit_key.setValidator(
@@ -26,12 +33,12 @@ class AlbertiDiscWidget(BaseQWidget):
         self.ui.button_make.clicked.connect(self._button_make_clicked)
 
     def _button_make_clicked(self) -> None:
-        """(Slot) Method for handling button click. (Encryption/decryption)"""
+        """Method - a slot for processing a signal when a button is pressed."""
         # Getting data from a form
         key = self.ui.line_edit_key.text()
         step = self.ui.spin_box_iteration_step.value()
         shift = self.ui.spin_box_key_alphabet_shift.value()
-        enc_proc = EncProc.from_str(self.ui.combo_box_mode.currentText())
+        enc_proc = EncProc.from_str(self.ui.combo_box_enc_proc.currentText())
 
         # We create an object, if an exception occurs, then we display an
         # error message and exit the method.
@@ -51,9 +58,11 @@ class AlbertiDiscWidget(BaseQWidget):
                 pass
 
     def _tab_text_processing(self, cipher: Alberti, enc_proc: EncProc) -> None:
-        """"""
+        """Method for encryption on the text processing tab."""
+        # Getting data from a form
         data = self.ui.text_edit_input.toPlainText()
 
+        # We call the "make" method, and pass data and processing type to it.
         try:
             processed_text = cipher.make(data, enc_proc)
 

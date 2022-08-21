fc302befe332efbc3182760c5b25c1370add01b6
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-08-21 16:54:33 +0700

    Added documentation to the hill.py module, as well as to the corresponding widget.

--- app/crypto/symmetric/hill.py
+++ app/crypto/symmetric/hill.py
@@ -1,3 +1,4 @@
+# This module contains the implementation of the cipher "Hill cipher"
 import math
 
 import numpy as np
@@ -9,11 +10,23 @@ from ..common import EncProc
 
 
 class HillError(Exception):
+    """The exception that is thrown when an error occurs in the Hill class"""
     pass
 
 
 class Hill:
-    def __init__(self, key: str, alphabet: str):
+    def __init__(self, key: str, alphabet: str) -> None:
+        """
+        Hill class constructor.
+
+        Args:
+            key: a string consisting of alphabetic characters. If the condition is not met,
+                an HillError exception will be thrown.
+
+            alphabet: a string consisting of various characters. The number of characters
+                in a string should preferably be a prime number, otherwise it may be necessary
+                to select a different size.
+        """
         if not key:
             raise HillError("The key is missing!")
 
@@ -29,6 +42,7 @@ class Hill:
         if len(alphabet) != len(set(alphabet)):
             raise HillError("The alphabet must be composed of unique characters!")
 
+        # Checking the conditions of the algorithm.
         n = math.isqrt(len(key))
         matrix_key = np.array(list(map(lambda x: alphabet.index(x.lower()), key))).reshape((n, n))
 
@@ -45,6 +59,16 @@ class Hill:
         self.alphabet = alphabet
 
     def _transform(self, text: str, enc_proc: EncProc) -> str:
+        """
+        Data encryption/decryption method.
+
+        Args:
+            text: the string to be encrypted or decrypted.
+            enc_proc: parameter responsible for the process of data encryption (encryption and decryption).
+
+        Returns:
+            Encrypted or decrypted string.
+        """
         if not text:
             raise HillError("Input text is empty!")
 
@@ -53,23 +77,28 @@ class Hill:
                 matrix_key = self.matrix_key
 
             case EncProc.DECRYPT:
+                # We are looking for the inverse matrix modulo and use it as a key.
                 matrix_key = np.array(Matrix(self.matrix_key).inv_mod(len(self.alphabet)))
 
             case _:
                 raise HillError(f"Invalid processing type! -> {enc_proc}")
 
+        # We take only those letters that satisfy the alphabet.
         letters, indices = get_letters_alphabetically(text, self.alphabet)
 
         n = math.isqrt(len(self.key))
 
         new_letters = ""
         for i in range(0, len(letters), n):
+            # We form a vector of indices and pad with zeros if there are not enough characters.
             vct = list(map(lambda x: self.alphabet.index(x.lower()), letters[i:i + n]))
             vct += [0 for _ in range(len(vct), n)]
 
+            # We multiply the key matrix by the vector and translate the indices into symbols.
             new_vct = np.matmul(matrix_key, vct) % len(self.alphabet)
             new_letters += "".join(map(lambda x: self.alphabet[x], new_vct))
 
+        # We place the encrypted letters in the places from which they were taken.
         text_list = list(text + new_letters[len(letters)::])
         for i, index in enumerate(indices):
             old_letter = text_list[index]
@@ -82,34 +111,39 @@ class Hill:
 
         return "".join(text_list)
 
-    def encrypt(self, text: str):
-        """Hill Cipher. Interface for calling encryption functions.
+    def encrypt(self, text: str) -> str:
+        """
+        Method - interface for encrypting input data.
 
         Args:
-            text: text to be encrypted.
+            text: the string to be encrypted.
 
         Returns:
             Encrypted string.
         """
         return self._transform(text, EncProc.ENCRYPT)
 
-    def decrypt(self, text: str):
-        """Hill Cipher. Interface for calling decryption functions.
+    def decrypt(self, text: str) -> str:
+        """
+        Method - interface for decrypting input data.
 
         Args:
-            text: text to be decrypted.
+            text: the string to be decrypted.
 
         Returns:
             Decrypted string.
         """
         return self._transform(text, EncProc.DECRYPT)
 
-    def make(self, text: str, enc_proc: EncProc.ENCRYPT):
-        """Hill Cipher. Interface for calling encryption/decryption functions.
+    def make(self, text: str, enc_proc: EncProc.ENCRYPT = EncProc.ENCRYPT) -> str:
+        """
+        Method - interface for encrypting/decrypting input data.
 
         Args:
-            text: text to be encrypted/decrypted.
-            enc_proc: encryption or decryption (default "encrypt").
+            text: the string to be encrypted or decrypted.
+
+            enc_proc: parameter responsible for the process of data encryption (encryption and decryption).
+                If the data object is of a different type, then an exception will be raised HillError.
 
         Returns:
             Encrypted or decrypted string.
--- app/gui/symmetric/hill/hill_widget.py
+++ app/gui/symmetric/hill/hill_widget.py
@@ -1,3 +1,5 @@
+# This module contains the implementation of the widget for working
+# with the encryption algorithm "Hill cipher".
 import string
 
 from PyQt6.QtWidgets import QMessageBox
@@ -12,21 +14,28 @@ from app.gui.widgets import BaseQWidget
 
 
 class HillWidget(BaseQWidget):
-    def __init__(self):
+    def __init__(self) -> None:
+        """HillWidget class constructor"""
         super(HillWidget, self).__init__()
         self.ui = Ui_Hill()
         self.ui.setupUi(self)
 
+        # Define the name of the widget that will be displayed in the list of widgets.
         self.title = "Hill"
 
+        # Initialization of possible encryption processes.
+        self.ui.combo_box_enc_proc.addItems((item.name.capitalize() for item in EncProc))
+
+        # Initialization of alphabet.
         self.ui.line_edit_alphabet.setText(string.ascii_lowercase + "!?,")
+
         self.ui.button_make.clicked.connect(self._button_make_clicked)
 
     def _button_make_clicked(self) -> None:
-        """Hill | (Slot) Method for handling button click. (Encryption/decryption)"""
+        """Method - a slot for processing a signal when a button is pressed."""
         key = self.ui.line_edit_key.text()
         alphabet = self.ui.line_edit_alphabet.text()
-        enc_proc = EncProc.from_str(self.ui.combo_box_mode.currentText())
+        enc_proc = EncProc.from_str(self.ui.combo_box_enc_proc.currentText())
 
         try:
             cipher = Hill(key, alphabet)
@@ -42,7 +51,8 @@ class HillWidget(BaseQWidget):
             case _:
                 pass
 
-    def _tab_text_processing(self, cipher: Hill, enc_proc: EncProc):
+    def _tab_text_processing(self, cipher: Hill, enc_proc: EncProc) -> None:
+        """Method for encryption on the text processing tab."""
         data = self.ui.text_edit_input.toPlainText()
 
         try:

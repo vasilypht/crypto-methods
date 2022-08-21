2894dea37b3f4305a1c9999622b59d068e0b1161
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-08-22 01:15:15 +0700

    Added documentation to the xor.py module, as well as to the corresponding widget.

--- app/crypto/symmetric/xor.py
+++ app/crypto/symmetric/xor.py
@@ -1,12 +1,19 @@
+# This module contains the implementation of the cipher "XOR cipher"
 from ..common import EncProc
 
 
 class XORError(Exception):
+    """The exception that is thrown when an error occurs in the XOR class"""
     pass
 
 
 class XOR:
     def __init__(self, key: str):
+        """
+
+        Args:
+            key: a string representing the 16th number.
+        """
         if not key:
             raise XORError("Key input value is empty!")
 
@@ -17,7 +24,28 @@ class XOR:
 
         self.index_key = 0
 
-    def _transform(self, data: bytes or str, enc_proc: EncProc, reset_state: bool = True):
+    def _transform(self, data: bytes or str, enc_proc: EncProc, reset_state: bool = True) -> str or bytes:
+        """
+        Method for processing data. This method converts the input data to bytes,
+        then conversions are performed and the output data is converted to a specific type.
+
+        * If the input is a string and the encryption mode, then the output will
+          be a string containing data in hexadecimal mode.
+
+        * If the input is bytes and the encryption or decryption mode, then the output will be bytes.
+
+        * If the input is a string (hex) and the decryption mode, then the output will be a
+          string with data in utf-8
+
+        Args:
+            data: bytes or string to be encrypted/decrypted.
+            enc_proc: parameter responsible for the process of data encryption (encryption and decryption).
+            reset_state: parameter indicating whether to reset the state before
+                encrypting/decrypting the input data.
+
+        Returns:
+            Encrypted or decrypted strings or bytes.
+        """
         if not data:
             raise XORError("The input data is empty!")
 
@@ -56,13 +84,65 @@ class XOR:
             case _:
                 raise XORError(f"Invalid processing type! -> {enc_proc}")
 
-    def encrypt(self, data: str or bytes, reset_state: bool = True):
+    def encrypt(self, data: str or bytes, reset_state: bool = True) -> str or bytes:
+        """
+        Method - interface for encrypting input data.
+
+        * If the input is a string and the encryption mode, then the output will
+          be a string containing data in hexadecimal mode.
+
+        * If the input is bytes and the encryption mode, then the output will be bytes.
+
+        Args:
+            data: bytes or string to be encrypted.
+            reset_state: parameter indicating whether to reset the state
+                before encrypting the input data.
+
+        Returns:
+            Encrypted strings or bytes.
+        """
         return self._transform(data, EncProc.ENCRYPT, reset_state)
 
-    def decrypt(self, data: str or bytes, reset_state: bool = True):
+    def decrypt(self, data: str or bytes, reset_state: bool = True) -> str or bytes:
+        """
+        Method - interface for encrypting input data.
+
+        * If the input is bytes and the decryption mode, then the output will be bytes.
+
+        * If the input is a string (hex) and the decryption mode, then the output will be a
+          string with data in utf-8
+
+        Args:
+            data: bytes or string to be decrypted.
+            reset_state: parameter indicating whether to reset the state
+                before decrypting the input data.
+
+        Returns:
+            Decrypted strings or bytes.
+        """
         return self._transform(data, EncProc.DECRYPT, reset_state)
 
-    def make(self, data: str or bytes, enc_proc: EncProc = EncProc.ENCRYPT, reset_state: bool = True):
+    def make(self, data: str or bytes, enc_proc: EncProc = EncProc.ENCRYPT, reset_state: bool = True) -> str or bytes:
+        """
+        Method - interface for encrypting/decrypting input data.
+
+        * If the input is a string and the encryption mode, then the output will
+          be a string containing data in hexadecimal mode.
+
+        * If the input is bytes and the encryption or decryption mode, then the output will be bytes.
+
+        * If the input is a string (hex) and the decryption mode, then the output will be a
+          string with data in utf-8
+
+        Args:
+            data: bytes or string to be encrypted/decrypted.
+            enc_proc: parameter responsible for the process of data encryption (encryption and decryption).
+            reset_state: parameter indicating whether to reset the state
+                before encrypting/decrypting the input data.
+
+        Returns:
+            Encrypted or decrypted strings or bytes.
+        """
         match enc_proc:
             case EncProc.ENCRYPT:
                 return self.encrypt(data, reset_state)
--- app/gui/symmetric/xor/xor_widget.py
+++ app/gui/symmetric/xor/xor_widget.py
@@ -1,3 +1,5 @@
+# This module contains the implementation of the widget for working
+# with the encryption algorithm "XOR".
 from PyQt6.QtWidgets import (
     QMessageBox,
     QMenu,
@@ -32,12 +34,18 @@ from app.gui.const import (
 
 class XORWidget(BaseQWidget):
     def __init__(self):
+        """XORWidget class constructor"""
         super(XORWidget, self).__init__()
         self.ui = Ui_XOR()
         self.ui.setupUi(self)
 
+        # Define the name of the widget that will be displayed in the list of widgets.
         self.title = "XOR"
 
+        # Initialization of possible encryption processes.
+        self.ui.combo_box_enc_proc.addItems((item.name.capitalize() for item in EncProc))
+
+        # Path received from dragdrop widget
         self.file_path = QUrl()
 
         # Add Drag and drop widget
@@ -57,13 +65,15 @@ class XORWidget(BaseQWidget):
 
         self.ui.button_make.clicked.connect(self._button_make_clicked)
 
+        # Binds to get the path to the file that is dropped into the widget
+        # and remove the path when the file is removed from the widget.
         self.drag_drop_widget.dropped.connect(self._file_path_changed)
         self.drag_drop_widget.canceled.connect(self._file_path_changed)
 
     def _button_make_clicked(self) -> None:
-        """XOR | (Slot) Method for handling button click. (Encryption/decryption)"""
+        """Method - a slot for processing a signal when a button is pressed."""
         key = self.gen_xor_key()
-        enc_proc = EncProc.from_str(self.ui.combo_box_mode.currentText())
+        enc_proc = EncProc.from_str(self.ui.combo_box_enc_proc.currentText())
 
         try:
             cipher = XOR(key)
@@ -82,7 +92,8 @@ class XORWidget(BaseQWidget):
             case _:
                 pass
 
-    def _tab_text_processing(self, cipher: XOR, enc_proc: EncProc):
+    def _tab_text_processing(self, cipher: XOR, enc_proc: EncProc) -> None:
+        """Method for encryption on the text processing tab."""
         data = self.ui.text_edit_input.toPlainText()
 
         try:
@@ -94,12 +105,13 @@ class XORWidget(BaseQWidget):
 
         self.ui.text_edit_output.setText(processed_data)
 
-    def _tab_document_processing(self, cipher: XOR, enc_proc: EncProc):
+    def _tab_document_processing(self, cipher: XOR, enc_proc: EncProc) -> None:
+        """Method for encryption on the document processing tab."""
         if self.file_path.isEmpty():
             QMessageBox.warning(self, "Warning!", "File not selected!")
             return
 
-        # get the name of the new file
+        # We get the path to the output file from the user.
         file_path_output, _ = QFileDialog.getSaveFileName(
             parent=self,
             caption="Save new file",
@@ -110,15 +122,19 @@ class XORWidget(BaseQWidget):
         if not file_path_output:
             return
 
+        # We create a stream object that will encrypt the contents of the file, then we send
+        # the object to the main window, which will launch it.
         thread_worker = FileProcessing(cipher, enc_proc, self.file_path.toLocalFile(), file_path_output)
         self.thread_ready.emit(thread_worker)
 
-    def _action_gen_iv_clicked(self):
+    def _action_gen_iv_clicked(self) -> None:
+        """Method for generating an initialization vector."""
         iv = np.random.randint(0, 256, self.ui.spin_box_iv_size.value())
         iv_hex = bytes(tuple(iv)).hex()
         self.ui.line_edit_iv.setText(iv_hex)
 
-    def _action_save_iv_clicked(self):
+    def _action_save_iv_clicked(self) -> None:
+        """Method for storing initialization vector."""
         if not self.ui.line_edit_iv.text():
             QMessageBox.warning(self, "Warning!", "The field with the IV is empty!")
             return
@@ -142,7 +158,8 @@ class XORWidget(BaseQWidget):
             QMessageBox.warning(self, "Warning!", "Failed to save file!")
             return
 
-    def _action_load_iv_clicked(self):
+    def _action_load_iv_clicked(self) -> None:
+        """Method for loading initialization vector."""
         filename, _ = QFileDialog.getOpenFileName(
             parent=self,
             caption="Open a key file",
@@ -171,12 +188,27 @@ class XORWidget(BaseQWidget):
 
         return bytes(next(rc4) for _ in range(self.ui.spin_box_gamma_size.value())).hex()
 
-    def _file_path_changed(self, file: QUrl):
+    def _file_path_changed(self, file: QUrl) -> None:
+        """Method - a slot for processing a signal from the dragdrop widget to get the path to the file."""
         self.file_path = file
 
 
 class FileProcessing(BaseQThread):
     def __init__(self, cipher: XOR, enc_proc: EncProc, input_file: str, output_file: str):
+        """
+        FileProcessing class constructor. This class is designed to encrypt
+        a file in a separate stream.
+
+        Args:
+            cipher: The cipher with which the file will be encrypted. This cipher
+                must have an interface "make".
+
+            enc_proc: parameter responsible for the process of data encryption
+                (encryption and decryption).
+
+            input_file: the path to the input file to be encrypted.
+            output_file: path to the output file to be written to.
+        """
         super(FileProcessing, self).__init__()
         self._cipher = cipher
         self._enc_proc = enc_proc
@@ -185,22 +217,31 @@ class FileProcessing(BaseQThread):
 
         self._is_worked = True
 
-    def close(self):
+    def close(self) -> None:
+        """Method for stopping a thread"""
+        # The flag is set to false and then we start to wait
+        # until the loop in the thread stops.
         self._is_worked = False
         self.wait()
 
     def run(self) -> None:
+        """The method that is called after the thread has started via the "start" method"""
         try:
             with open(self._input_file, "rb") as input_file, \
                     open(self._output_file, "wb") as output_file:
+                # Find out the file size (number of bytes - if binary format,
+                # number of characters - if normal)
                 input_file.seek(0, 2)
                 input_file_size = input_file.tell()
                 input_file.seek(0, 0)
 
+                # Initializes the progress bar by sending signals to the main window.
                 self.pbar.emit((PBarCommands.SET_RANGE, 0, input_file_size))
                 self.pbar.emit((PBarCommands.SET_VALUE, 0))
                 self.pbar.emit((PBarCommands.SHOW,))
 
+                # We read a piece of data, encrypt it and write it to the output file,
+                # simultaneously updating the value in the progress bar.
                 while (block := input_file.read(MAX_BYTES_READ)) and self._is_worked:
                     encrypted_block = self._cipher.make(block, self._enc_proc, reset_state=False)
                     output_file.write(encrypted_block)
@@ -208,9 +249,11 @@ class FileProcessing(BaseQThread):
                     self.pbar.emit((PBarCommands.SET_VALUE, input_file.tell()))
 
         except Exception as e:
+            # If an exception occurs, we send an error message.
             self.message.emit("An error occurred while working with files or when "
                               "determining the file size. (Check encryption mode)\n"
                               f"({e.args[0]})")
 
         finally:
+            # Close the processbar.
             self.pbar.emit((PBarCommands.CLOSE,))

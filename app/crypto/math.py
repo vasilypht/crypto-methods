5937401de35769c28d95bb016889d206015dcec4
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-09-10 20:03:39 +0700

    Added ext_gcd

--- app/crypto/math.py
+++ app/crypto/math.py
@@ -16,3 +16,26 @@ def fpow(a, n, m=None) -> int:
         n >>= 1
 
     return result
+
+
+def ext_gcd(a, b):
+    s0, s1 = 1, 0
+    t0, t1 = 0, 1
+
+    swap_flag = False
+    if a < b:
+        a, b = b, a
+        swap_flag = True
+
+    while b:
+        q = a // b
+        a, b = b, a % b
+        s0, s1 = s1, s0 - s1 * q
+        t0, t1 = t1, t0 - t1 * q
+
+    if swap_flag:
+        answer = (a, t0, s0)
+    else:
+        answer = (a, s0, t0)
+
+    return answer

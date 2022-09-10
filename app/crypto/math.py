b45aecf28616db4789bb1d2a9286b141b7dc8116
Vasily Perekhrest <perekhrest-vasily@mail.ru>
2022-09-10 19:34:02 +0700

    Added the function of fast exponentiation modulo.
    Co-authored-by: KennyMustD1e no-reply

---
+++ app/crypto/math.py
@@ -0,0 +1,17 @@
+
+def fpow(a, n, m=None) -> int:
+    result = 1
+
+    while n > 0:
+        if n & 1:
+            result *= a
+            if m:
+                result %= m
+
+        a **= 2
+        if m:
+            a %= m
+
+        n >>= 1
+
+    return result
